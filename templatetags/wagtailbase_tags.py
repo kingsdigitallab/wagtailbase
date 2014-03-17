6adac6d69c04903b614acd7f29369f2b820f3b6e
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2014-03-17 16:41:34 +0000

    Adding template tags. Work in progress.

---
+++ templatetags/wagtailbase_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+
+@register.assignment_tag(takes_context=True)
+def get_site_root(context):
+    """Returns the site root Page, not the implementation-specific model used.
+
+    :rtype: `wagtail.wagtailcore.models.Page`
+    """
+    return 'hello'
