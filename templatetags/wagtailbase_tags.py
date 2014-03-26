068eef5b9f5223ec56a3ad81d95c8d3b6547dc44
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2014-03-26 11:30:19 +0000

    Added a tag to check if a page should display a local menu.

--- templatetags/wagtailbase_tags.py
+++ templatetags/wagtailbase_tags.py
@@ -41,41 +41,24 @@ def get_site_root(context):
     return context['request'].site.root_page
 
 
+@register.assignment_tag(takes_context=True)
+def has_local_menu(context, current_page):
+    """Returns True if the current page has a local menu, False otherwise. A
+    page has a local menu, if it is not the site root, and if it is not a leaf
+    page."""
+    site_root = get_site_root(context)
+
+    if current_page != site_root and not current_page.is_leaf():
+        return True
+
+    return False
+
+
 @register.filter
 def is_current_or_ancestor(page, current_page):
     """Returns True if the given page is the current page or is an ancestor of
     the current page."""
     return current_page.is_current_or_ancestor(page)
-
-
-@register.inclusion_tag('wagtailbase/tags/main_menu.html', takes_context=True)
-def main_menu(context, root, current_page):
-    """Returns the main menu items, the children of the root page. Only live
-    pages that have the show_in_menus setting on are returned."""
-    menu_pages = root.get_children().filter(live=True, show_in_menus=True)
-
-    return {'request': context['request'], 'root': root,
-            'current_page': current_page, 'menu_pages': menu_pages}
-
-
-@register.inclusion_tag('wagtailbase/tags/local_menu.html', takes_context=True)
-def local_menu(context, current_page=None):
-    """Retrieves the secondary links for the 'also in this section' links -
-    either the children or siblings of the current page."""
-    menu_pages = []
-
-    if current_page:
-        menu_pages = current_page.get_children().filter(
-            live=True, show_in_menus=True)
-
-        # if no children, get siblings instead
-        if len(menu_pages) == 0:
-            menu_pages = current_page.get_siblings().filter(
-                live=True, show_in_menus=True)
-
-    # required by the pageurl tag that we want to use within this template
-    return {'request': context['request'], 'current_page': current_page,
-            'menu_pages': menu_pages}
 
 
 @register.inclusion_tag('wagtailbase/tags/latest_blog_post.html',
@@ -91,3 +74,33 @@ def latest_blog_post(context, parent=None):
         post = BlogPost.objects.all().order_by('-date').first()
 
     return {'request': context['request'], 'post': post}
+
+
+@register.inclusion_tag('wagtailbase/tags/local_menu.html', takes_context=True)
+def local_menu(context, current_page=None):
+    """Retrieves the secondary links for the 'also in this section' links -
+    either the children or siblings of the current page."""
+    menu_pages = []
+
+    if current_page:
+        menu_pages = current_page.get_children().filter(
+            live=True, show_in_menus=True)
+
+        # if no children, get siblings instead
+        if len(menu_pages) == 0 and not current_page.is_leaf():
+            menu_pages = current_page.get_siblings().filter(
+                live=True, show_in_menus=True)
+
+    # required by the pageurl tag that we want to use within this template
+    return {'request': context['request'], 'current_page': current_page,
+            'menu_pages': menu_pages}
+
+
+@register.inclusion_tag('wagtailbase/tags/main_menu.html', takes_context=True)
+def main_menu(context, root, current_page):
+    """Returns the main menu items, the children of the root page. Only live
+    pages that have the show_in_menus setting on are returned."""
+    menu_pages = root.get_children().filter(live=True, show_in_menus=True)
+
+    return {'request': context['request'], 'root': root,
+            'current_page': current_page, 'menu_pages': menu_pages}
